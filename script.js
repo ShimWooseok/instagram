5f65e3740f36492298e3a6fd4d544dd8c1b734f6
swss <zaqplm7913@naver.com>
2023-03-11 07:24:20 +0900

    23.03.11

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".slide").slick({
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: "linear",
+      autoplay: true
+    });
+  });
+  
